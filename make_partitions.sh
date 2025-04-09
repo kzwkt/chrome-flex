367ba3a75569f118bde1ccbaa4a3fcd53e5b52bc
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-04-09 10:10:08 +0545

    Update make_partitions.sh

--- make_partitions.sh
+++ make_partitions.sh
@@ -2,7 +2,7 @@
 
 # cros_size:[16k,512b,1g,512b,512b,64m,64m,16m,64m,4g,4g,flexible)
 # cgpt add -b start_block -s size_in_blocks -t part_type -l part_label -i part_id part/address
-
+device=sda
 b=32768
 end=124207128
 sector_size=$(cat /sys/class/block/$device/queue/logical_block_size)
